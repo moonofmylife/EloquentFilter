83070d353ce38e8876d0c00a9ffa92ee6ceca17a
Eric Tucker <tucker.ericm@gmail.com>
2016-06-29 22:34:40 -0700

    update with develop

--- tests/ModelFilterChildTest.php
+++ tests/ModelFilterChildTest.php
@@ -10,6 +10,18 @@ use Mockery as m;
 
 class ModelFilterChildTest extends TestCase
 {
+    protected $model;
+
+    public function setUp()
+    {
+        $this->model = new User;
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
     public function testGetRelatedModel()
     {
         $userMock = m::mock('User');
@@ -27,8 +39,17 @@ class ModelFilterChildTest extends TestCase
         $this->assertEquals($client, new Client);
     }
 
-    public function tearDown()
+    public function testProvideFilter()
     {
-        m::close();
+        // Empty provide filter App\ModelFilters is the default namespace when empty
+        $this->assertEquals($this->model->provideFilter(), App\ModelFilters\UserFilter::class);
+        // Filter Value
+        $this->assertEquals($this->model->provideFilter(App\ModelFilters\DynamicFilter\TestModelFilter::class), App\ModelFilters\DynamicFilter\TestModelFilter::class);
+    }
+
+    public function testGetModelFilterClass()
+    {
+        $this->assertEquals($this->model->getModelFilterClass(), EloquentFilter\TestClass\UserFilter::class);
+
     }
 }

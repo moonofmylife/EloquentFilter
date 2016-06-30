916fedbb77df37166945285903f41562b9a6a749
Eric Tucker <tucker.ericm@gmail.com>
2016-06-29 22:01:55 -0700

    import mock namespaces

--- tests/ModelFilterChildTest.php
+++ tests/ModelFilterChildTest.php
@@ -4,6 +4,8 @@ use PHPUnit\Framework\TestCase;
 use EloquentFilter\TestClass\User;
 use EloquentFilter\TestClass\Client;
 use EloquentFilter\TestClass\UserFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Mockery as m;
 
 class ModelFilterChildTest extends TestCase
@@ -11,8 +13,8 @@ class ModelFilterChildTest extends TestCase
     public function testGetRelatedModel()
     {
         $userMock = m::mock('User');
-        $userQueryMock = m::mock('Illuminate\Database\Eloquent\Builder');
-        $hasManyMock = m::mock('Illuminate\Database\Eloquent\Relations\HasMany');
+        $userQueryMock = m::mock('');
+        $hasManyMock = m::mock('');
 
         $userQueryMock->shouldReceive('getModel')->once()->andReturn($userMock);
 

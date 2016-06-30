9974ada32868e1c31665dfa746cef78f6006717c
Eric Tucker <tucker.ericm@gmail.com>
2016-06-29 22:40:46 -0700

    formatting

--- tests/ModelFilterChildTest.php
+++ tests/ModelFilterChildTest.php
@@ -1,12 +1,6 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-<<<<<<< HEAD
-use TestClasses\TestModel;
-use Mockery as m;
-
-
-=======
 use EloquentFilter\TestClass\User;
 use EloquentFilter\TestClass\Client;
 use EloquentFilter\TestClass\UserFilter;
@@ -14,16 +8,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Mockery as m;
 
->>>>>>> master
 class ModelFilterChildTest extends TestCase
 {
     protected $model;
 
     public function setUp()
     {
-<<<<<<< HEAD
-        $this->model = new TestModel;
-=======
         $this->model = new User;
     }
 
@@ -47,29 +37,18 @@ class ModelFilterChildTest extends TestCase
         $client = (new UserFilter($userQueryMock))->getRelatedModel('clients');
 
         $this->assertEquals($client, new Client);
->>>>>>> master
     }
 
     public function testProvideFilter()
     {
-<<<<<<< HEAD
-        // Empty provide filter
-        $this->assertEquals($this->model->provideFilter(), App\ModelFilters\TestModelFilter::class);
-=======
         // Empty provide filter App\ModelFilters is the default namespace when empty
         $this->assertEquals($this->model->provideFilter(), App\ModelFilters\UserFilter::class);
->>>>>>> master
         // Filter Value
         $this->assertEquals($this->model->provideFilter(App\ModelFilters\DynamicFilter\TestModelFilter::class), App\ModelFilters\DynamicFilter\TestModelFilter::class);
     }
 
     public function testGetModelFilterClass()
     {
-<<<<<<< HEAD
-        $this->assertEquals($this->model->getModelFilterClass(), FILTER_CLASS);
-=======
         $this->assertEquals($this->model->getModelFilterClass(), EloquentFilter\TestClass\UserFilter::class);
-
->>>>>>> master
     }
 }

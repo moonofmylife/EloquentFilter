318efbeb74867c20304cbe4bb3f889b536060019
Eric Tucker <tucker.ericm@gmail.com>
2017-11-25 13:24:06 -0800

    Add page parameter to simplePaginate

--- src/Filterable.php
+++ src/Filterable.php
@@ -61,13 +61,14 @@ trait Filterable
      * @param  int $perPage
      * @param  array $columns
      * @param  string $pageName
+     * @param int|null $page
      * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      *
      * @throws \InvalidArgumentException
      */
-    public function scopeSimplePaginateFilter($query, $perPage = null, $columns = ['*'], $pageName = 'page')
+    public function scopeSimplePaginateFilter($query, $perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
     {
-        $paginator = $query->simplePaginate($perPage, $columns, $pageName);
+        $paginator = $query->simplePaginate($perPage, $columns, $pageName, $page);
         $paginator->appends($this->filtered);
 
         return $paginator;
